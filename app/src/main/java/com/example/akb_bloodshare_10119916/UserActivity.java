f486fff1ef29fd7c4fa8c743d44221758faca3d6
DianArpian <arpiandian7@gmail.com>
2022-05-08 23:29:06 +0700

    ini adalah pesan commit pertama

---
+++ app/src/main/java/com/example/akb_bloodshare_10119916/UserActivity.java
@@ -0,0 +1,15 @@
+package com.example.akb_bloodshare_10119916;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+
+}
